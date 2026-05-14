8d735ba7131ade84bbe5cc002b928651c62a9578
CST <CST_TECH@mini-stations-Mac-mini.local>
2026-05-14 12:16:54 +0530

    fixed build issue 23

--- src/services/reel.service.js
+++ src/services/reel.service.js
@@ -1,5 +1,6 @@
 import Reel from "../models/Reel.js";
 import Category from "../models/Category.js";
+import Client from "../models/Client.js";
 import ApiError from "../utils/ApiError.js";
 import { uniqueSlug } from "../utils/slugify.js";
 import { buildQuery } from "../utils/queryBuilder.js";
